71e509f701d9106e1f7ecc8ce28bb584a7972586
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-12 09:23:36 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,18 @@
+// String Data Type
+public class StringDemo {
+    public static void main(String[] args){
+        // Alpha values
+        String name="Anisha Patil";
+        System.out.println(name);
+
+        // Alpha Numeric values
+        String str= "anisha11";
+        System.out.println(str);
+
+        // Alpha Numeric Symbolic values
+        String email="anishapatil05@gmail.com";
+        System.out.println(email); 
+
+     
+    }
+}
