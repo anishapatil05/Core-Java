51dd468cfcc742e00f51926a8afc83f8c066609d
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-19 07:31:49 +0530

    Add files via upload

---
+++ Staticvar.java
@@ -0,0 +1,16 @@
+public class Staticvar {
+    static String Company_name="Apple";
+    static String CEO ="Tim cook";
+      int service_no;
+      String product_name;
+    public static void main(String[] args){
+         System.out.println(Company_name);
+         System.out.println(CEO);
+          
+         Staticvar sc = new Staticvar();
+         sc.service_no = 6786;
+         sc.product_name="IPHONE 16";
+         System.out.println(sc.service_no);
+         System.out.println(sc.product_name);
+    }
+}
