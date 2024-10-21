7a8d942f5d58abda6a16015905931b1b730894b3
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-21 08:05:22 +0530

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,16 @@
+/*  Comments in java: 
+                       1. Single-line Comment
+                       2. Multi-line Comment    */   
+public class Comments {
+       public static void main(String[] args) {
+       /* Calling addition method in main 
+                    method   */
+        addition(6, 8);
+    }
+    // Addition method
+    public static void addition(int a, int b)
+    {
+        System.out.print("addition is: ");
+        System.out.println(a+b);
+    }
+}
