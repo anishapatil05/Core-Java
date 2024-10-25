93b15803e8e8e9ac2b84600f5e0f2cf7f7d3fed2
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-25 09:57:58 +0530

    Add files via upload

---
+++ Packages/sum.java
@@ -0,0 +1,16 @@
+package addition;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 sum s = new sum();
+		 s.add(9, 5);
+	}
+	public void add(int a , int b)
+	{
+	 int res=a+b;
+	 System.out.println("sum is: "+res);
+	 }
+
+}
