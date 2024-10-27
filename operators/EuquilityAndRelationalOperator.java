94f2441458e4f6269d486af50bbdfe32b48d7920
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-27 15:07:34 +0530

    Add files via upload

---
+++ operators/EuquilityAndRelationalOperator.java
@@ -0,0 +1,44 @@
+package operators;
+
+public class EuquilityAndRelationalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*****EQUAL TO OPERTAOR: == ****");
+		int a = 10;
+		int b = 11;
+		System.out.println(a == b);
+		int a1= 11;
+		System.out.println(a1 == b);
+		
+		System.out.println("***** NOT EQUALS TO OPERTAOR: != *****");
+		int k =90;
+		int j = 56;
+		System.out.println(k != j);
+		int p =90;
+		System.out.println(k != p);
+		
+		System.out.println("***** GRETER THAN OPERTAOR: > *****");
+		int yourSalary = 50000;
+		int friendSalary = 30000;
+		System.out.println(yourSalary>friendSalary);
+		System.out.println(friendSalary>yourSalary);
+		
+		System.out.println("***** GRETER THAN OR EQUAL TO OPERTAOR: >= *****");
+		int sal1 = 40001;
+		int sal2 = 4000;
+		System.out.println(sal1 >= sal2);
+		System.out.println(sal2 > sal1);
+		System.out.println(sal1 = sal2);
+		
+		System.out.println("***** LESS THAN OR EQUAL TO OPERTAOR: <= *****");
+		int val1 = 300;
+		int val2 = 489;
+		System.out.println(val1 < val2);
+		System.out.println(val2 <= val1);
+		System.out.println(val1 = val2);
+	
+			
+	}
+
+}
