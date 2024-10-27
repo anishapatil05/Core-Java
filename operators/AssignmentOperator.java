1ffb420bec8829290b7e5bfe227d6654ac98b995
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-27 15:05:49 +0530

    Add files via upload

---
+++ operators/AssignmentOperator.java
@@ -0,0 +1,12 @@
+//  Assignment Operator -- 1. = (equal to)
+package operators;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int a = 10;
+         System.out.println(a);
+	}
+
+}
