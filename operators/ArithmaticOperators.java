143c813a88bff23d467f06872fb4bc75dc74fee4
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-27 15:05:11 +0530

    Add files via upload

---
+++ operators/ArithmaticOperators.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class ArithmaticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("----Arithmatic Operators---- ");
+		
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+	}
+
+}
