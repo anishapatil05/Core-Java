e19449f6e21ea39e5c86232d97207745efafc9f3
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-29 21:36:04 +0530

    Add files via upload

---
+++ operators/InstanceofOperator.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class InstanceofOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Student s = new Student();
+	}
+
+}
