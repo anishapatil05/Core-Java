f837ffa807836c9051a95afc8af582b13190c351
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-29 21:36:28 +0530

    Add files via upload

---
+++ operators/BitwiseOperator.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class BitwiseOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("****BITWISE OPERATOR****");
+        int a= 5;
+//        Unary bitwise complement : ~
+        System.out.println(~a);
+        
+        
+	}
+
+}
