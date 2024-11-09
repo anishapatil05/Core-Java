4b4fcbaf1c1cc7bec7ac8cfa831800cd21707875
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:22:05 +0530

    Update AddTwoValues.java

--- Packages/AddTwoValues.java
+++ Packages/AddTwoValues.java
@@ -1,15 +1,14 @@
 // Package sumation call the method add from addition package sum class 
 package sumation;
 
+// importing method sum() from different package (addition) of class sum...
 import addition.sum;
 
 public class AddTwoValues {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// calling non-static method sum() from addition package...
 		 sum s3 = new sum();
-		 s3.add(9, 5);
-		 
+		 s3.add(9, 5);	 
 	}
-
 }
