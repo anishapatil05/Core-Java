7f6b17cba168b6e70ce326a4e11712eda040b68b
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:34:00 +0530

    Update WhileLoop.java

--- Looping-Statements/WhileLoop.java
+++ Looping-Statements/WhileLoop.java
@@ -1,21 +1,23 @@
-
+// Example of while loop
 public class WhileLoop {
 
 	public static void main(String[] args) {
-	
+
+		// initilization
 		int value = value();
-		
-		while(value<=100)
+
+		// Condition (value<=100)
+		while(value<=100) 
 		{
 			System.out.println(value);
+			// inc/dec (value++ / value--)
 			value++;
 		}
 		System.out.println(value);
 	}
-
-	public static int value()
+	// method value()
+       public static int value()
 	{
 		return 4;
 	}
-
 }
