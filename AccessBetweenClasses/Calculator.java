cf41c46c534298773706bb2b7fdd3433284e01c0
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:43:56 +0530

    Update Calculator.java

--- AccessBetweenClasses/Calculator.java
+++ AccessBetweenClasses/Calculator.java
@@ -1,4 +1,4 @@
-// Accessing the methods from different class
+// 1. Creating Calculator class...
 public class Calculator {
     public static void main(String[] args) {
         // Accessing static m in same class
@@ -10,11 +10,13 @@ public class Calculator {
         result =c.sub(56, 4);
         System.out.println(result);
     }
+    // addition method
     public static int addition(int a, int b)
     {
         System.out.println("Addition method of calculator class");
         return a+b;
     }
+     // sub method
     public int sub(int a, int b)
     {
         System.out.println("Subtraction method of calculator class");
