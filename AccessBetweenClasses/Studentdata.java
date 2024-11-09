ff9a7437be183af0b5185a49c767bd15a969a26a
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:57:16 +0530

    Update Studentdata.java

--- AccessBetweenClasses/Studentdata.java
+++ AccessBetweenClasses/Studentdata.java
@@ -1,10 +1,11 @@
 // Example of :  Accessibilty of variables from different classes
 public class Studentdata {
     public static void main(String[] args) {
-        // Accessing non-static variable clgname of Student class...
+        // Accessing static variable clgname of Student class...
         String name = Student.clgname;
         System.out.println(name);
 
+         // Accessing non-static variable clgcount of Student class...
         Student s1 = new Student();
         System.out.println(s1.clgcount);
     }
