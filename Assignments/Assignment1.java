f15e6ba8c9e1eea92c929cf6b237bf782ab77f44
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 08:43:40 +0530

    Add files via upload

---
+++ Assignments/Assignment1.java
@@ -0,0 +1,20 @@
+// Assignment 1
+public class Assignment1 {
+    public static void main(String[] anisha) {
+        Assignment1 obj = new Assignment1();
+        obj.addition(89.765, 1234l);
+    }
+
+    public void addition(double a, long b) {
+        System.out.println("Addition is: ");
+        System.out.println(a + b);
+
+        sub(90, 65);
+
+    }
+
+    public static void sub(int a, int b) {
+        System.out.println("Subtraction is: ");
+        System.out.println(a - b);
+    }
+}
