db8fdff0e0513450f8192ff98e3a887fce71fb44
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 08:59:30 +0530

    Update LocalVar.java

--- Variable_Types/LocalVar.java
+++ Variable_Types/LocalVar.java
@@ -1,9 +1,10 @@
-// Local Variabels
+// Example of Local Var
 public class LocalVar {
+      // static Variabels
       static int a=67;
       static int b=9;
     public static void main(String[] args) {
-           
+           // Local Variable
           int result = 0;
           result = a+b;
         System.out.println(result);
