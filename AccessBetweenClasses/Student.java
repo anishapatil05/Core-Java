ebe50279bfa1b9d2300f8c93103fdd6a42657e52
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 08:30:38 +0530

    Add files via upload

---
+++ AccessBetweenClasses/Student.java
@@ -0,0 +1,14 @@
+// Accessiblity of  Variabls from different classes
+public class Student {
+    static String clgname="JJMCOE";
+     int clgcount=890;
+
+     public static void main(String[] anisha)
+     { 
+        // Accessing from same class
+        System.out.println(clgname);
+
+        Student s1 = new Student();
+        System.out.println(s1.clgcount);
+     }
+}
