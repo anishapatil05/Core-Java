051f97dd27a5fe1d797ba9621df4c15a748eabd7
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:01:14 +0530

    Update Staticvar.java

--- Variable_Types/Staticvar.java
+++ Variable_Types/Staticvar.java
@@ -1,13 +1,18 @@
-// Static Variables
+// Example of Static Variables
 public class Staticvar {
+    // Static Variables
     static String Company_name="Apple";
     static String CEO ="Tim cook";
+    // instance var 
       int service_no;
       String product_name;
+    
     public static void main(String[] args){
+        // Accessing Static variable
          System.out.println(Company_name);
          System.out.println(CEO);
-          
+
+        // Accessing non-static variable
          Staticvar sc = new Staticvar();
          sc.service_no = 6786;
          sc.product_name="IPHONE 16";
