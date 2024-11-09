cbcfdb14569003f2351dc92fadcdad849735829d
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:03:28 +0530

    Update InstanceVar.java

--- Variable_Types/InstanceVar.java
+++ Variable_Types/InstanceVar.java
@@ -1,13 +1,16 @@
-// Instance Variable
+// Example of Instance Variable
 public class InstanceVar {
+    // non-static Variable
     String name;
     int roll_no;
     int age;
     public static void main(String[] args)
     {
+        // Accessing non-static Variable
         InstanceVar iVar = new InstanceVar();
         iVar.name="Anisha";
         System.out.println(iVar.name);
+        
         // give default values of data-type int 
         System.out.println(iVar.roll_no);
         System.out.println(iVar.age);
