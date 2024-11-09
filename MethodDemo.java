80810bdf82ae124cff6ef1be189e0968464925df
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:13:17 +0530

    Update MethodDemo.java

--- MethodDemo.java
+++ MethodDemo.java
@@ -1,25 +1,33 @@
+//  Introduction of Methods....
 public class MethodDemo {
     public static void main(String[] args) {
-       long m_num=mobilenum();
+        // method calling
+       long m_num = mobilenum();
        System.out.println(m_num);
-
-       printname();
-
-       double value=anyvalue();
+        // (m_num : 908765 , mobilenum() == 908765 )
+    
+        printname();
+        // (  printname() == "Anisha Patil" || value does't Printed of return type)
+        
+       double value = anyvalue();
        System.out.println(value);
+        // (value: 89.65 )
 
     }
+    // method mobilenum()
     public static long mobilenum()
      {
         System.out.println("908765");
         return 908765;
      } 
+      // method printname()
      public static String printname()
      {
-        System.out.println("ANisha Patil");
+        System.out.println("Anisha Patil");
         return "akp";
      }
-     public static  double anyvalue()
+      // method  anyvalue()
+     public static double anyvalue()
      {
         return 89.65;
      }
