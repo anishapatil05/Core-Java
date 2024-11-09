7ec5030621c1df91e98212d6ddadfb474f6fcc54
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:49:39 +0530

    Update CalculatorResult.java

--- AccessBetweenClasses/CalculatorResult.java
+++ AccessBetweenClasses/CalculatorResult.java
@@ -1,23 +1,24 @@
-// Accessing methods from calculator class
+// Example of Accessing methods of calculator class
 public class CalculatorResult {
   public static void main(String[] args) {
-    // Accessing Static Addition method of calculator class
+    // Accessing Static addition() of calculator class
        int result =Calculator.addition(78, 9);
        System.out.println(result);
 
-   // Accessing non-static sub method of calculator class
+   // Accessing non-static sub() of calculator class
       Calculator c = new Calculator();
        result = c.sub(8, 3);
        System.out.println(result);
 
+    // Accessing static printsubtractionResult() in same class
        CalculatorResult CR = new CalculatorResult();
        CR.printsubtractionResult();
   }  
   public void printsubtractionResult()
   {
-    // Accessing non-static method of calculato class
+    // Accessing non-static sub() of calculator class
       Calculator c1 = new Calculator();
-      int result=c1.sub(7, 1);
+      int result = c1.sub(7, 1);
       System.out.println(result);
 
   }
