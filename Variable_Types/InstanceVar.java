57977f9d6e6989ea57d3037dd93e91101512237b
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 08:57:10 +0530

    Add files via upload

---
+++ Variable_Types/InstanceVar.java
@@ -0,0 +1,17 @@
+// Instance Variable
+public class InstanceVar {
+    String name;
+    int roll_no;
+    int age;
+    public static void main(String[] args)
+    {
+        InstanceVar iVar = new InstanceVar();
+        iVar.name="Anisha";
+        System.out.println(iVar.name);
+        // give default values of data-type int 
+        System.out.println(iVar.roll_no);
+        System.out.println(iVar.age);
+        
+    }
+
+}
