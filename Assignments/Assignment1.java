4ffde0e7a75f06374c1f15df857f7253096cf139
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:36:45 +0530

    Update Assignment1.java

--- Assignments/Assignment1.java
+++ Assignments/Assignment1.java
@@ -1,10 +1,12 @@
-// Assignment 1
+// Assignment 1 : Passing Arguments
 public class Assignment1 {
     public static void main(String[] anisha) {
+        
         Assignment1 obj = new Assignment1();
         obj.addition(89.765, 1234l);
     }
 
+    // method addition()
     public void addition(double a, long b) {
         System.out.println("Addition is: ");
         System.out.println(a + b);
@@ -12,7 +14,7 @@ public class Assignment1 {
         sub(90, 65);
 
     }
-
+    // method sub()
     public static void sub(int a, int b) {
         System.out.println("Subtraction is: ");
         System.out.println(a - b);
