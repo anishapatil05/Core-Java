0ed05d51af1cfcfb5ce3a7c7a54bacb60cf15e29
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 09:25:15 +0530

    Update sum.java

--- Packages/sum.java
+++ Packages/sum.java
@@ -1,17 +1,17 @@
-// Addition Package
+// addition Package
 package addition;
 
 public class sum {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+           public static void main(String[] args) {
+             //  calling non-static method...           
 		 sum s = new sum();
 		 s.add(9, 5);
 	}
+	// non-static method add(int a , int b)
 	public void add(int a , int b)
 	{
-	 int res=a+b;
-	 System.out.println("sum is: "+res);
-	 }
+	   int res=a+b;
+	   System.out.println("sum is: "+res);
+	}
 
 }
