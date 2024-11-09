fc2457693ce7e44a5b84f3221c79baf4f2f06b63
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-11-09 08:56:38 +0530

    Add files via upload

---
+++ Variable_Types/LocalVar.java
@@ -0,0 +1,17 @@
+// Local Variabels
+public class LocalVar {
+      static int a=67;
+      static int b=9;
+    public static void main(String[] args) {
+           
+          int result = 0;
+          result = a+b;
+        System.out.println(result);
+        subtraction();
+    }
+    public static void subtraction()
+    {
+        int result = a-b;
+        System.out.println(result);
+    } 
+}
