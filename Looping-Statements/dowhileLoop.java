b740d3b5d0363077a19a49c1825592a5936228ab
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-12-25 07:36:23 +0530

    Add files via upload

---
+++ Looping-Statements/dowhileLoop.java
@@ -0,0 +1,15 @@
+
+// Example of do-while loop
+public class dowhileLoop {
+
+	public static void main(String[] args) {
+
+		int counter = 0;
+		do {
+			System.out.println("value of counter is: " + counter);
+			counter++;
+		} while (counter <= 4);
+
+	}
+
+}
