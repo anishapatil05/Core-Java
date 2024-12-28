8988f955816056cbed1eebbdfe65fc3488cbedd0
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-12-28 16:26:21 +0530

    Add files via upload

---
+++ OOP/inheritance/Multi-Level-Inheritance/MultiLevel1.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class MultiLevel1 extends MultiLevel {
+	
+// Perforn Subtraction
+	public int sub(int a, int b, int c) {
+		return a - b - c;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
