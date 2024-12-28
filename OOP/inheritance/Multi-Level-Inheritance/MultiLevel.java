ad1cc9ea91ea8f227b116c3b3d4f47a26127f9f7
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-12-28 16:25:51 +0530

    Add files via upload

---
+++ OOP/inheritance/Multi-Level-Inheritance/MultiLevel.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+//  Example of Multilevel Inheritance
+public class MultiLevel {
+	
+//  Perform Addition
+	public int add(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
