7f5e7acb16f91698df6ce6976f598742b944415b
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-12-28 16:30:23 +0530

    Add files via upload

---
+++ OOP/inheritance/Hierarchical-Level-Inheritance/HierachicalLevel.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+// Example of HierachicalLevel INheritance: Base class
+public class HierachicalLevel {
+	
+	public int calculatoradd(int a, int b)
+	{
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
