98abee6d3cc200ae40c3d149a6f9b748a3ced88f
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-06 18:51:30 +0530

    Add files via upload

---
+++ OOP/Abstraction/Inheritance/Single-Level/Calculator1.java
@@ -0,0 +1,13 @@
+package anisha.abstraction;
+
+// Inheritance in Interface
+// 1. Single level Inheritance
+//Interface 1 : Calculator1 & Interface 1 : Calculator 2
+public interface Calculator1 {
+
+	public void add(int a , int b);
+	
+	public  void sub(int a, int b);
+	
+	
+}
