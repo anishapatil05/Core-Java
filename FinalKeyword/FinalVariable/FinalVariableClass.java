bb6400f91813628ecf3f834f84a4ff56e060b7a9
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-06 19:09:28 +0530

    Add files via upload

---
+++ FinalKeyword/FinalVariable/FinalVariableClass.java
@@ -0,0 +1,25 @@
+package anisha.finalkeyword;
+
+public class FinalVariableClass {
+
+//	final variables : after intilizining the var it does not modify in a class
+	final String dateOfBirth = "5/7/2004";
+	String companyName = "sony";
+
+	public void printingInfo() {
+//		dateOfBirth var : does not modify because it is final var
+		System.out.println(dateOfBirth);
+		System.out.println(companyName);
+
+//		 we can change the companyName bcz it's not a final 
+		companyName = "apple";
+		System.out.println(companyName);
+	}
+
+	public static void main(String[] args) {
+		FinalVariableClass obj = new FinalVariableClass();
+		obj.printingInfo();
+
+	}
+
+}
