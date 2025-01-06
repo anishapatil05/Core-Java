2000d5cbfc90041f769468ce731d891de7455898
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-06 19:09:49 +0530

    Add files via upload

---
+++ FinalKeyword/FinalVariable/FinalVarInterface.java
@@ -0,0 +1,10 @@
+package anisha.finalkeyword;
+
+public interface FinalVarInterface {
+
+//	By default the variables in Interface final and static
+	String name = "anisha";
+	int age = 87;
+
+	public void method1();
+}
