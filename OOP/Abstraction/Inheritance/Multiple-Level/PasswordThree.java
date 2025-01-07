62ea4701107a46906a3b067b9f4b27bc1a5e9056
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-07 08:16:38 +0530

    Add files via upload

---
+++ OOP/Abstraction/Inheritance/Multiple-Level/PasswordThree.java
@@ -0,0 +1,6 @@
+package anisha.abstraction;
+
+//Example of Multiple Inheritance in Interfaces
+public interface PasswordThree extends PasswordOne, PasswordTwo {
+	public void showPasswordOptions();
+}
