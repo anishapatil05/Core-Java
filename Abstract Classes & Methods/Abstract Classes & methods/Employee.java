4e0d110575be17102a9b858d6bf9dcd47a68bda9
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-09 18:46:13 +0530

    Add files via upload

---
+++ Abstract Classes & Methods/Abstract Classes & methods/Employee.java
@@ -0,0 +1,21 @@
+package com.anisha.abstractmc;
+
+// Example of Abstract classes
+public interface Employee {
+	public static final String NAME = "anisha";
+	public static final double SALARY = 10000;
+	public static final String DEPARTMENT = "devlopment";
+
+//	abstract method
+	public void work();
+
+//	abstract method
+	public void conductMeeting();
+
+//	abstract method
+	public void getDetails();
+
+//	abstract method
+	public void updateDetails();
+
+}
