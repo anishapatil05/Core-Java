a15fd581a83409b2eea819bf212e02a126649e77
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-09 18:44:30 +0530

    Add files via upload

---
+++ Abstract Classes & Methods/Example1/BookTypes.java
@@ -0,0 +1,14 @@
+package com.anisha.abstractmc;
+
+public  abstract class BookTypes {
+      public void book1()
+      {
+    	  System.out.println("this is english book");
+      }
+      
+//      abstract method
+      public abstract String book2();
+      
+//      abstract method
+      public abstract String book3();
+}
