32dd81a694e81aa221dac26f2a64fbdd7cc3cc50
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-12 10:26:03 +0530

    Add files via upload

---
+++ "this" & "super" keyword/"this" keyword/Employee.java
@@ -0,0 +1,16 @@
+package com.anisha.thiskeyword;
+
+// Example of this & super keyword
+public class Employee {
+	String name="anisha";
+	int age =21;
+        public void work()
+        {
+        	System.out.println("Employee class : Development ");
+        }
+        public void salary()
+        {
+        	System.out.println("salary: 30000");
+        }
+	
+}
