2f9954b4be552325840843e8e15654832a06afb4
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-15 19:01:41 +0530

    Add files via upload

---
+++ OOP/Polymorphism/Compile-RunTime Polymorphism/RunTime Polymorphism/PaymentApplication.java
@@ -0,0 +1,17 @@
+
+// Runtime Polymorphism
+public class PaymentApplication {
+	
+    
+	public void makePayment(double ammount)
+	{
+		System.out.println("making payment by different ways");
+	}
+	
+	public void getDetails(String name,long passbooknumber)
+	{
+		System.out.println("name: "+name+","+"Passbook Number: "+passbooknumber);
+	}
+	
+	
+}
