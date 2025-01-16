e8c7960e8d9d78927602a06aa05e0db5cf4cb6b8
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-16 07:41:25 +0530

    Add files via upload

---
+++ OOP/Polymorphism/Compile-RunTime Polymorphism/Runtime Polymorphism/Example2/AnimalBehaviour.java
@@ -0,0 +1,8 @@
+package com.anisha.runtimepolymorphism.examples;
+
+// interface : AnimalBehaviour 
+public interface AnimalBehaviour {
+//	Abstract method
+	public void makeSound(String animalType);
+   
+}
