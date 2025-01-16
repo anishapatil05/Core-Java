ea78f3e40cc6f67acd0adba4a2b422f11a542fe0
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-16 07:13:28 +0530

    Add files via upload

---
+++ OOP/Polymorphism/Compile-RunTime Polymorphism/Runtime Polymorphism/NetBankingApplication.java
@@ -0,0 +1,13 @@
+
+public class NetBankingApplication extends PaymentApplication {
+
+    public void makePayment(double ammount,String type) {
+    
+    	System.out.println("Payment done by Net Banking Application");
+    }
+    
+    public void ChekingInfo(String password)
+    {
+    	System.out.println("Password: "+password);
+    }
+}
