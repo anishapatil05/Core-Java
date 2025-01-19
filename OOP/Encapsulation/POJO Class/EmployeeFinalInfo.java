9881fdeb4ceee8af605e9bec8060fe6f39f092b3
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-19 10:10:47 +0530

    Add files via upload

---
+++ OOP/Encapsulation/POJO Class/EmployeeFinalInfo.java
@@ -0,0 +1,13 @@
+package com.anisha.pojo;
+
+public class EmployeeFinalInfo {
+
+	public void empDtails(Empolyee emp) {
+		System.out.println(emp.getName());
+		System.out.println(emp.getId());
+		System.out.println(emp.getDepartment());
+		System.out.println(emp.getSalary());
+
+	}
+
+}
