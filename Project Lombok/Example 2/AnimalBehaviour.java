a3cc0fbb61c2fd463aab7954a292ce8492da82c5
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-21 22:04:16 +0530

    Add files via upload

---
+++ Project Lombok/Example 2/AnimalBehaviour.java
@@ -0,0 +1,12 @@
+package com.anisha.lombok;
+
+public class AnimalBehaviour {
+
+	public static void main(String[] args) {
+Animal behaviour = new Animal("cat", "mouse", "mehw-mehw", "home", 5);
+System.out.println(behaviour);
+
+
+	}
+
+}
