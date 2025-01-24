5abe80fa3d3242969a61926c250468446d1f8e4d
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-24 09:04:37 +0530

    Add files via upload

---
+++ Type Casting/Non-primitive data type/Student1.java
@@ -0,0 +1,14 @@
+package com.anisha.typecasting;
+
+public class Student1 extends Student {
+	int id;
+	public int student1Info()
+	{
+		return id;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
