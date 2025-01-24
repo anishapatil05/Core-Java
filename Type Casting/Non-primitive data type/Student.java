0a5eb0d1100af0004b48fc0ad4206107f04e66ff
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-24 09:04:14 +0530

    Add files via upload

---
+++ Type Casting/Non-primitive data type/Student.java
@@ -0,0 +1,10 @@
+package com.anisha.typecasting;
+
+// Example of type casting : Non-Primitive data type
+public class Student {
+
+	public void printInfo(String name)
+	{
+		System.out.println(name);
+	}
+}
