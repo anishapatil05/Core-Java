eb53a49b353db6535b783f4f4d7735a46ab291be
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-29 07:39:40 +0530

    Add files via upload

---
+++ Annotations/Predefined-Annotations/Employee.java
@@ -0,0 +1,14 @@
+package com.anisha.annotations.predifined;
+
+public class Employee {
+//	Predefined Annotations
+//	@Override
+
+	public void salary() {
+		System.out.println("Salary is According to Devlopment");
+	}
+
+	public String name() {
+		return "Anisha";
+	}
+}
