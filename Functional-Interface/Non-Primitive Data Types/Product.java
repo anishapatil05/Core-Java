7114f018be3c0f0de1774793b2406b57d16170f5
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-01-31 21:28:09 +0530

    Add files via upload

---
+++ Functional-Interface/Non-Primitive Data Types/Product.java
@@ -0,0 +1,8 @@
+package com.anisha.functional.Interface.lambdaexp;
+
+@FunctionalInterface
+public interface Product {
+
+	// Passing non-primitive parameters as reference
+	public void productInfo(ProductDetails prod);
+}
