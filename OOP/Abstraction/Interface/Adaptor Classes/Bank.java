4c86dd26c6a4dd6c007ef4490b3baf25f07e3e0a
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-02 09:52:29 +0530

    Add files via upload

---
+++ OOP/Abstraction/Interface/Adaptor Classes/Bank.java
@@ -0,0 +1,9 @@
+package com.anisha.adaptorclasses;
+
+public interface Bank {
+   public void credit(double ammount);
+   
+   public void debit(double ammount);
+   
+   public void checkBalance();
+}
