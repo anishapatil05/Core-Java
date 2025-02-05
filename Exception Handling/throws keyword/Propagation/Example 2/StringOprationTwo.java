96f66ae16a44436ef8fbfe06797c68dd413bba74
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-05 11:17:48 +0530

    Add files via upload

---
+++ Exception Handling/throws keyword/Propagation/Example 2/StringOprationTwo.java
@@ -0,0 +1,15 @@
+package com.anisha.eception.throwkeyword;
+
+public class StringOprationTwo {
+
+	public static void main(String[] args) throws Exception{
+		StringOprations obj = new StringOprations();
+	
+//		1 handle : try-catch  2.Propagate 
+		// It give you option 
+			System.out.println(obj.opration("Another main method my logic: NISHA"));
+			System.out.println(obj.arrayOpration());
+		
+	}
+
+}
