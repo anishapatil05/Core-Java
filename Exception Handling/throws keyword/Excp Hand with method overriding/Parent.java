103730dc7c04f3b6b8e73fe47c072c472475d56c
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-08 14:04:01 +0530

    Add files via upload

---
+++ Exception Handling/throws keyword/Excp Hand with method overriding/Parent.java
@@ -0,0 +1,9 @@
+package com.anisha.eceptionhandling.trycatch.blocks;
+
+// Cheking child allows overriding method with Exception
+public class Parent {
+    void method1()
+    {
+    	System.out.println("parent method");
+    }
+}
