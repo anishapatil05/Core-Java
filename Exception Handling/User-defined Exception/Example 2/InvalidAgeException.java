1e303461f3df3fb874453e653a968b7242c7d13d
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-08 15:36:26 +0530

    Update InvalidAgeException.java

--- Exception Handling/User-defined Exception/Example 2/InvalidAgeException.java
+++ Exception Handling/User-defined Exception/Example 2/InvalidAgeException.java
@@ -1,5 +1,6 @@
 package com.anisha.userdifined.exceptiontype;
 
+ // Userdefined Checked Exception by extending Exception class
 public class InvalidAgeException extends Exception {
 
 //	Default constructor
