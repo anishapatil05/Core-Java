e67457f758b02ee82094a9f25e2309999c98a656
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-08 15:22:19 +0530

    Add files via upload

---
+++ Exception Handling/User-defined Exception/Example 2/InvalidAgeException.java
@@ -0,0 +1,10 @@
+package com.anisha.userdifined.exceptiontype;
+
+public class InvalidAgeException extends Exception {
+
+//	Default constructor
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+
+}
