9a0fdc684fd55fe0115d717a2a0500d9d6a70314
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-13 14:32:27 +0530

    Add files via upload

---
+++ Collection Framework/ArrayList/Example/Employee.java
@@ -0,0 +1,16 @@
+package com.anisha.collectionfw.arrayList.demo;
+
+public class Employee {
+
+	public String ename;
+	public int eid;
+	
+	
+	public Employee(String ename, int eid) {
+		super();
+		this.ename = ename;
+		this.eid = eid;
+	}
+
+
+}
