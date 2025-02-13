b9e8d237b52d92a370d0f7e7fef8534d6883a0ba
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-13 14:32:55 +0530

    Add files via upload

---
+++ Collection Framework/ArrayList/Example/Student.java
@@ -0,0 +1,30 @@
+package com.anisha.collectionfw.arrayList.demo;
+
+public class Student {
+	
+	public String sname;
+	public int sid;
+	
+	
+
+	public Student(String sname, int sid) {
+		super();
+		this.sname = sname;
+		this.sid = sid;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Student [sname=" + sname + ", sid=" + sid + "]";
+	}
+
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
