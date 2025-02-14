127757dd2df84f45b496e73e59a34e9a7baf7e61
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-14 17:00:20 +0530

    Add files via upload

---
+++ Collection Framework/ArrayList/Cursors/Book.java
@@ -0,0 +1,15 @@
+package com.anisha.collectionfw.cursors;
+
+public class Book {
+
+	int id;
+	String name;
+	String auther;
+
+	public Book(int id, String name, String auther) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.auther = auther;
+	}
+}
