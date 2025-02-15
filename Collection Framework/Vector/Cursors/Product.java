ad8f5a3e2c7803c29c702bb87565a7c38ea25964
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-15 08:51:44 +0530

    Add files via upload

---
+++ Collection Framework/Vector/Cursors/Product.java
@@ -0,0 +1,16 @@
+package com.anisha.collectionfw.vector.cursors;
+
+public class Product {
+
+	public String name;
+	public int id;
+	public double cost;
+
+	public Product(String name, int id, double cost) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.cost = cost;
+	}
+
+}
