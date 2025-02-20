327a8f2032147856bcc2cb3b25440ad455547260
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-20 07:09:34 +0530

    Add files via upload

---
+++ Collection Framework/Collection Sorting/Comparator Sorting/Employee.java
@@ -0,0 +1,14 @@
+package com.anisha.collectionfw.sorting.comparator;
+
+public class Employee {
+
+	String name;
+	int id;
+
+	public Employee(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+
+}
