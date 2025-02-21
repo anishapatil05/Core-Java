5eb3464ea8c504b9f45bfd56a5252d547f249488
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-21 12:51:48 +0530

    Add files via upload

---
+++ Collection Framework/Set Interface/TreeHashSet/Customized Sorting/Product.java
@@ -0,0 +1,16 @@
+package com.anisha.customized;
+
+public class Product {
+	
+	 String name;
+	 int id;
+	 double cost;
+	public Product(String name, int id, double cost) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.cost = cost;
+	}
+	 
+	 
+}
