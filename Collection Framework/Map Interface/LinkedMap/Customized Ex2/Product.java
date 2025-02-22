2dc42f66cdcb563ca933e3d148c1b22b8b53e8c3
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-22 19:26:00 +0530

    Add files via upload

---
+++ Collection Framework/Map Interface/LinkedMap/Customized Ex2/Product.java
@@ -0,0 +1,14 @@
+package com.anisha.collectionfw.linkedhashmap.ex;
+
+public class Product {
+ 
+	String name;
+	int id;
+	public Product(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	
+	
+}
