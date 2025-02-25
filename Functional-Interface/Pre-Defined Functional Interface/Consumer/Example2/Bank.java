e84086977b164d2bc7a9acab243bde4b9a1b0c0d
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-25 21:47:16 +0530

    Add files via upload

---
+++ Functional-Interface/Pre-Defined Functional Interface/Consumer/Example2/Bank.java
@@ -0,0 +1,16 @@
+package com.anisha.consumer;
+
+public class Bank {
+
+	public double ammount;
+	public int time;
+	public int roi;
+
+	public Bank(double ammount, int time, int roi) {
+		super();
+		this.ammount = ammount;
+		this.time = time;
+		this.roi = roi;
+	}
+
+}
