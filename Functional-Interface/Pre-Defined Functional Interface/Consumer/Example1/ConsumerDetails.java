359f156b24dc20ae6df9cf67e4d639d789fac480
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-25 21:43:04 +0530

    Add files via upload

---
+++ Functional-Interface/Pre-Defined Functional Interface/Consumer/Example1/ConsumerDetails.java
@@ -0,0 +1,20 @@
+package com.anisha.consumer;
+
+public class ConsumerDetails {
+
+ public String name;
+ public String orderName;
+ public double orderPrice;
+ public int orderId;
+ 
+public  ConsumerDetails(String name, String orderName, double orderPrice, int orderId) {
+	super();
+	this.name = name;
+	this.orderName = orderName;
+	this.orderPrice = orderPrice;
+	this.orderId = orderId;
+}
+ 
+ 
+ 
+}
