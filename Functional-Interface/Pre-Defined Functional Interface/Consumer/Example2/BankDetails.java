f3ea4ceab46da45d23089f10c37a07edbdc2da35
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-25 21:47:55 +0530

    Add files via upload

---
+++ Functional-Interface/Pre-Defined Functional Interface/Consumer/Example2/BankDetails.java
@@ -0,0 +1,30 @@
+package com.anisha.consumer;
+
+import java.util.function.Consumer;
+
+public class BankDetails {
+
+	
+	//REQ : Calculating EMI value
+	
+	
+	public static void main(String[] args) {
+
+		//public Bank(double ammount, int time, int roi) {
+		Bank ICIC = new Bank(45000, 4, 9);
+		Consumer<Bank> EMIValue = input -> {
+
+			double monthalyRate = (input.roi / 100.0) / 12;
+           double numratorEMI = (input.ammount * monthalyRate *Math.pow(1 + monthalyRate ,input.time));
+			
+           double denomintorEMI = Math.pow( 1+ monthalyRate, input.time)-1;
+           
+           double finalEMIValue = numratorEMI / denomintorEMI;
+           System.out.println("EMI Ammount is " + finalEMIValue);
+		};
+
+		EMIValue.accept(ICIC);
+
+	}
+
+}
