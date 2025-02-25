d6e5b4210799f1fabd0c872e9f2331b52442f993
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-02-25 21:49:08 +0530

    Add files via upload

---
+++ Functional-Interface/Pre-Defined Functional Interface/Consumer/Example3/EmployeeEmail.java
@@ -0,0 +1,14 @@
+package com.anisha.consumer;
+
+public class EmployeeEmail {
+
+	public String name;
+	public String emailId;
+
+	public EmployeeEmail(String name, String emailId) {
+		super();
+		this.name = name;
+		this.emailId = emailId;
+	}
+
+}
