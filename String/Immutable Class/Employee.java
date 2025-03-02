7dbf40dec7feaa234e84b181a5fbb20139d400cd
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-02 19:42:04 +0530

    Add files via upload

---
+++ String/Immutable Class/Employee.java
@@ -0,0 +1,16 @@
+package com.anisha.string.imutableclass;
+
+public class Employee {
+
+	private final String name;
+
+	public Employee(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+}
