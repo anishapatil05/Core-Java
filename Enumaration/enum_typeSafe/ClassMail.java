a87abf2ce0054f486ea3cb7d35829a7239dcf39b
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-05 14:51:18 +0530

    Add files via upload

---
+++ Enumaration/enum_typeSafe/ClassMail.java
@@ -0,0 +1,8 @@
+package com.anisha.enumeration.typesafty;
+
+public class ClassMail {
+
+	public static final int INBOX = 1;
+	public static final int COMPOSE = 2;
+	public static final int SENT = 3;
+}
