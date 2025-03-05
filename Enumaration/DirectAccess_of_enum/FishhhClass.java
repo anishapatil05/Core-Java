47be42aab3193bc233fda4f0366cdae9e4ac8c6d
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-05 14:54:42 +0530

    Add files via upload

---
+++ Enumaration/DirectAccess_of_enum/FishhhClass.java
@@ -0,0 +1,15 @@
+package com.anisha.enumeration.pack1;
+
+// using static keyword we accessing enum names directly
+import static com.anisha.enumeration.pack2.FishEnum.*;
+
+public class FishhhClass {
+
+	public static void main(String[] args) {
+
+		System.out.println(GOLD);
+		System.out.println(STAR);
+
+	}
+
+}
