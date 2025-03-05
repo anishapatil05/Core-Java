bb2123da78e31d1c3d4fe0b3cb9d8aaac15ff34d
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-05 15:00:28 +0530

    Add files via upload

---
+++ Enumaration/enum examples/Example1/CalanderImp.java
@@ -0,0 +1,11 @@
+package com.anisha.enumeration;
+
+public class CalanderImp {
+
+	public static void main(String[] args) {
+		CalanderMonths mon = CalanderMonths.FEB;
+		System.out.println(mon);
+
+	}
+
+}
