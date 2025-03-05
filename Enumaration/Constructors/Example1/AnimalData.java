f41e0293fab7b0c35c5248b10fbbc0adf1d85175
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-05 14:42:47 +0530

    Add files via upload

---
+++ Enumaration/Constructors/Example1/AnimalData.java
@@ -0,0 +1,14 @@
+package com.anisha.enumeration.constructors;
+
+public class AnimalData {
+
+	public static void main(String[] args) {
+		
+	 Animal[] aa = Animal.values();
+		for(Animal aaa : aa ) {
+			System.out.println(aaa);
+		}
+
+	}
+
+}
