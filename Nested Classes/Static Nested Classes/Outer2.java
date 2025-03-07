a580107afb833dc1b361940491817bcebd5c7e86
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-07 07:02:26 +0530

    Add files via upload

---
+++ Nested Classes/Static Nested Classes/Outer2.java
@@ -0,0 +1,25 @@
+package com.anisha.nestedclasses.staticnestedclass;
+
+// Static Nested Class : able to add main method inside Inner class
+
+public class Outer2 {
+
+	static class Inner {
+
+		public static void main(String[] args) {
+			System.out.println("this is static nested class method");
+		}
+
+	}
+}
+
+
+
+//static void disp() {
+//System.out.println("this is static nested class method");
+//}
+//
+//public static void main(String[] args) {
+//
+//disp();
+//}
