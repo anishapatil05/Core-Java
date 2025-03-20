bc838af81e871a0580bc343d10a4587c02566d66
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-20 09:04:06 +0530

    Add files via upload

--- Multi-threading/Thread Methods/MyThread.java
+++ Multi-threading/Thread Methods/MyThread.java
@@ -1,45 +1,23 @@
-package com.anisha.multithreading.methods;
+package com.anisha.multithreading.interuppted;
 
 public class MyThread extends Thread {
 
 	public void run() {
+		for (int i = 0; i < 6; i++) {
+			System.out.println("User Thread");
 
-//		isDaemon()
-		System.out.println("is Daemon thread " + isDaemon());
-
-		int i = 0;
-		while (i < 5) {
-//			getName()
-			System.out.println("Current thread " + Thread.currentThread().getName());
-			System.out.println("Multiplication " + 5 * i);
-			i++;
-
-//			sleep()
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(2000);
 			} catch (InterruptedException e) {
-
-				throw new RuntimeException();
+				System.out.println("Interupted exception call");
 			}
-
 		}
 	}
 
 	public static void main(String[] args) {
-
 		MyThread t1 = new MyThread();
-//		setName()
-		t1.setName("first thread");
 		t1.start();
-		try {
-			t1.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		MyThread t2 = new MyThread();
-		t2.setName("second thread");
-		t2.start();
+		t1.interrupt();
 
 	}
 
