2ca6ed916f0f7c9ac8042785b20d51c4c548f3d0
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-20 09:02:25 +0530

    Add files via upload

---
+++ Multi-threading/Thread Class/MainThread.java
@@ -0,0 +1,13 @@
+package com.anisha.multithreading;
+
+//Single Thread Model
+public class MainThread {
+
+	public static void main(String[] args) {         //Thread excution starting 
+		
+		System.out.println("hello world");                     // Thread excutes
+		System.out.println(Thread.currentThread().getName());
+
+	}       //Thread Excution Complete
+
+}
