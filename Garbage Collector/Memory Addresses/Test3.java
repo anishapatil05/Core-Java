db7aec59d74870d46ed9921dd8a9610356cfc1c4
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-03-20 09:26:17 +0530

    Add files via upload

---
+++ Garbage Collector/Memory Addresses/Test3.java
@@ -0,0 +1,22 @@
+package com.anisha.grabagecollector.memoryaddess;
+
+import java.io.IOException;
+
+public class Test3 {
+
+	public static void main(String[] args) throws IOException {
+
+		Runtime r = Runtime.getRuntime();
+
+//		Open notepad
+		r.exec("notepad");
+//		r.exec("mspaint");
+//		r.exec("calc");
+
+//		Used  to Shutdown or Restart the System in Java
+//		r.exec("shutdown -s -t 0");
+//		r.exec("shutdown -r -t 0");
+
+	}
+
+}
